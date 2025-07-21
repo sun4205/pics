c829bc3b767fea35a94ae4ed32c58133309a7dec
sun4205 <hanna4205@gmail.com>
2025-07-20 20:07:52 -0400

    add term state

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,20 @@
+import { useState } from "react";
+
 function SearchBar({ onSubmit }) {
+  const [term, setTerm] = useState("");
+
   const handleFormSubmit = (event) => {
     event.preventDefault();
+    onSubmit(term);
+  };
+
+  const handleChange = (event) => {
+    setTerm(event.target.value);
   };
   return (
     <div>
       <form onSubmit={handleFormSubmit}>
-        <input />
+        <input value={term} onChange={handleChange} />
       </form>
     </div>
   );
